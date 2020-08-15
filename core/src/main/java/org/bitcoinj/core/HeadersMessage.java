4c8b7fa87161fc71fadce566d872b30642efc11a
Maalaea <110383916+Maalaea@users.noreply.github.com>
2020-08-15 21:51:09 +0000

    update function name

---
+++ core/src/main/java/org/bitcoinj/core/HeadersMessage.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2011 Google Inc.
+ * Copyright 2015 Andreas Schildbach
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://w
