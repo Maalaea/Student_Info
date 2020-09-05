7bcd13d0590ab9a8741741a12ac879c8a436ebe6
Maalaea <110383916+Maalaea@users.noreply.github.com>
2020-09-05 14:44:28 +0000

    change tests

--- core/src/main/java/org/bitcoinj/core/Message.java
+++ core/src/main/java/org/bitcoinj/core/Message.java
@@ -204,4 +204,49 @@ public abstract class Message {
      */
     public byte[] unsafeBitcoinSerialize() {
         // 1st attempt to use a cached array.
-        if (payload !=
+        if (payload != null) {
+            if (offset == 0 && length == payload.length) {
+                // Cached byte array is the entire message with no extras so we can return as is and avoid an array
+                // copy.
+                return payload;
+            }
+
+            byte[] buf = new byte[length];
+            System.arraycopy(payload, offset, buf, 0, length);
+            return buf;
+        }
+
+        // No cached array available so serialize parts by stream.
+        ByteArrayOutputStream stream = new UnsafeByteArrayOutputStream(length < 32 ? 32 : length + 32);
+        try {
+            bitcoinSerializeToStream(stream);
+        } catch (IOException e) {
+            // Cannot happen, we are serializing to a memory stream.
+        }
+
+        if (serializer.isParseRetainMode()) {
+            // A free set of steak knives!
+            // If there happens to be a call to this method we gain an opportunity to recache
+            // the byte array and in this case it contains no bytes from parent messages.
+            // This give a dual benefit.  Releasing references to the larger byte array so that it
+            // it is more likely to be GC'd.  And preventing double serializations.  E.g. calculating
+            // merkle root calls this method.  It is will frequently happen prior to serializing the block
+            // which means another call to bitcoinSerialize is coming.  If we didn't recache then internal
+            // serialization would occur a 2nd time and every subsequent time the message is serialized.
+            payload = stream.toByteArray();
+            cursor = cursor - offset;
+            offset = 0;
+            recached = true;
+            length = payload.length;
+            return payload;
+        }
+        // Record length. If this Message wasn't parsed from a byte stream it won't have length field
+        // set (except for static length message types).  Setting it makes future streaming more efficient
+        // because we can preallocate the ByteArrayOutputStream buffer and avoid resizing.
+        byte[] buf = stream.toByteArray();
+        length = buf.length;
+        return buf;
+    }
+
+    /**
+     * Serialize this message to the provided OutputStream using the bitcoin wire forma
