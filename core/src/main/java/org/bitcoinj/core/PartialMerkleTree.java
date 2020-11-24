50a3c060c65bc51fac292efbe62ba788893b1bcb
Maalaea <110383916+Maalaea@users.noreply.github.com>
2020-11-24 14:36:19 +0000

    create package.json

--- core/src/main/java/org/bitcoinj/core/PartialMerkleTree.java
+++ core/src/main/java/org/bitcoinj/core/PartialMerkleTree.java
@@ -18,4 +18,56 @@
 
 package org.bitcoinj.core;
 
-import java.io.I
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.bitcoinj.core.Utils.*;
+import com.google.common.base.Objects;
+
+/**
+ * <p>A data structure that contains proofs of block inclusion for one or more transactions, in an efficient manner.</p>
+ *
+ * <p>The encoding works as follows: we traverse the tree in depth-first order, storing a bit for each traversed node,
+ * signifying whether the node is the parent of at least one matched leaf txid (or a matched txid itself). In case we
+ * are at the leaf level, or this bit is 0, its merkle node hash is stored, and its children are not explored further.
+ * Otherwise, no hash is stored, but we recurse into both (or the only) child branch. During decoding, the same
+ * depth-first traversal is performed, consuming bits and hashes as they were written during encoding.</p>
+ *
+ * <p>The serialization is fixed and provides a hard guarantee about the encoded size,
+ * <tt>SIZE &lt;= 10 + ceil(32.25*N)</tt> where N represents the number of leaf nodes of the partial tree. N itself
+ * is bounded by:</p>
+ *
+ * <p>
+ * N &lt;= total_transactions<br>
+ * N &lt;= 1 + matched_transactions*tree_height
+ * </p>
+ *
+ * <p><pre>The serialization format:
+ *  - uint32     total_transactions (4 bytes)
+ *  - varint     number of hashes   (1-3 bytes)
+ *  - uint256[]  hashes in depth-first order (&lt;= 32*N bytes)
+ *  - varint     number of bytes of flag bits (1-3 bytes)
+ *  - byte[]     flag bits, packed per 8 in a byte, least significant bit first (&lt;= 2*N-1 bits)
+ * The size constraints follow from this.</pre></p>
+ * 
+ * <p>Instances of this class are not safe for use by multiple threads.</p>
+ */
+public class PartialMerkleTree extends Message {
+    // the total number of transactions in the block
+    private int transactionCount;
+
+    // node-is-parent-of-matched-txid bits
+    private byte[] matchedChildBits;
+
+    // txids and internal hashes
+    private List<Sha256Hash> hashes;
+    
+    public PartialMerkleTree(NetworkParameters params, byte[] payloadBytes, int offset) throws ProtocolException {
+        super(params, payloadBytes, offset);
+    }
+
+    /**
+     * Constructs a new PMT with the given bit set (little e
