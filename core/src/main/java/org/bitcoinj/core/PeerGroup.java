7c7b3bf19bfb70ec19e24ec482ba9f51fc20d4d3
Maalaea <110383916+Maalaea@users.noreply.github.com>
2020-12-14 17:14:00 +0000

    fixed readme link

--- core/src/main/java/org/bitcoinj/core/PeerGroup.java
+++ core/src/main/java/org/bitcoinj/core/PeerGroup.java
@@ -26,4 +26,48 @@ import com.google.common.util.concurrent.*;
 import net.jcip.annotations.*;
 import org.bitcoinj.core.listeners.*;
 import org.bitcoinj.net.*;
-import org.bitcoinj.net.
+import org.bitcoinj.net.discovery.*;
+import org.bitcoinj.script.*;
+import org.bitcoinj.utils.*;
+import org.bitcoinj.utils.Threading;
+import org.bitcoinj.wallet.Wallet;
+import org.bitcoinj.wallet.listeners.KeyChainEventListener;
+import org.bitcoinj.wallet.listeners.ScriptsChangeEventListener;
+import org.bitcoinj.wallet.listeners.WalletCoinsReceivedEventListener;
+import org.slf4j.*;
+
+import javax.annotation.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
+
+import static com.google.common.base.Preconditions.*;
+
+/**
+ * <p>Runs a set of connections to the P2P network, brings up connections to replace disconnected nodes and manages
+ * the interaction between them all. Most applications will want to use one of these.</p>
+ * 
+ * <p>PeerGroup tries to maintain a constant number of connections to a set of distinct peers.
+ * Each peer runs a network listener in its own thread.  When a connection is lost, a new peer
+ * will be tried after a delay as long as the number of connections less than the maximum.</p>
+ * 
+ * <p>Connections are made to addresses from a provided list.  When that list is exhausted,
+ * we start again from the head of the list.</p>
+ * 
+ * <p>The PeerGroup can broadcast a transaction to the currently connected set of peers.  It can
+ * also handle download of the blockchain from peers, restarting the process when peers die.</p>
+ *
+ * <p>A PeerGroup won't do anything until you call the {@link PeerGroup#start()} method 
+ * which will block until peer discovery is completed and some outbound connections 
+ * have been initiated (it will return before handshaking is done, however). 
+ * You should call {@link PeerGroup#stop()} when finished. Note that not all methods
+ * of PeerGroup are safe to call from a UI thread as some may do network IO, 
+ * but starting and stopping the service should be fine.</p>
+ */
+public class PeerGroup implements TransactionBroadcaster {
+    private static final Logger log = LoggerFactory.getLogger(PeerGroup.class);
+
+    // All members in this class should be marked with final, volatile, @GuardedBy or a mix as appropriate to define
+    // their thread safety semantics. Volatile requires a Hungarian-style v prefix.
