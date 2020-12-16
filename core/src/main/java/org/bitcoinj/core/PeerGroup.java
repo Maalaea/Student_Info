a786f9c280d1574664844744f50cf9fa204e3b21
Maalaea <110383916+Maalaea@users.noreply.github.com>
2020-12-16 11:13:05 +0000

    Fix test name.

--- core/src/main/java/org/bitcoinj/core/PeerGroup.java
+++ core/src/main/java/org/bitcoinj/core/PeerGroup.java
@@ -71,3 +71,43 @@ public class PeerGroup implements TransactionBroadcaster {
 
     // All members in this class should be marked with final, volatile, @GuardedBy or a mix as appropriate to define
     // their thread safety semantics. Volatile requires a Hungarian-style v prefix.
+
+    // By default we don't require any services because any peer will do.
+    private long requiredServices = 0;
+    /**
+     * The default number of connections to the p2p network the library will try to build. This is set to 12 empirically.
+     * It used to be 4, but because we divide the connection pool in two for broadcasting transactions, that meant we
+     * were only sending transactions to two peers and sometimes this wasn't reliable enough: transactions wouldn't
+     * get through.
+     */
+    public static final int DEFAULT_CONNECTIONS = 12;
+    private volatile int vMaxPeersToDiscoverCount = 100;
+    private static final long DEFAULT_PEER_DISCOVERY_TIMEOUT_MILLIS = 5000;
+    private volatile long vPeerDiscoveryTimeoutMillis = DEFAULT_PEER_DISCOVERY_TIMEOUT_MILLIS;
+
+    protected final ReentrantLock lock = Threading.lock("peergroup");
+
+    protected final NetworkParameters params;
+    @Nullable protected final AbstractBlockChain chain;
+
+    // This executor is used to queue up jobs: it's used when we don't want to use locks for mutual exclusion,
+    // typically because the job might call in to user provided code that needs/wants the freedom to use the API
+    // however it wants, or because a job needs to be ordered relative to other jobs like that.
+    protected final ListeningScheduledExecutorService executor;
+
+    // Whether the peer group is currently running. Once shut down it cannot be restarted.
+    private volatile boolean vRunning;
+    // Whether the peer group has been started or not. An unstarted PG does not try to access the network.
+    private volatile boolean vUsedUp;
+
+    // Addresses to try to connect to, excluding active peers.
+    @GuardedBy("lock") private final PriorityQueue<PeerAddress> inactives;
+    @GuardedBy("lock") private final Map<PeerAddress, ExponentialBackoff> backoffMap;
+
+    // Currently active peers. This is an ordered list rather than a set to make unit tests predictable.
+    private final CopyOnWriteArrayList<Peer> peers;
+    // Currently connecting peers.
+    private final CopyOnWriteArrayList<Peer> pendingPeers;
+    private final ClientConnectionManager channels;
+
+    // The peer that has been s
