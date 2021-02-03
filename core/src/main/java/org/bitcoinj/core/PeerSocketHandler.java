68bcbe8579780f95cbeca50116006be7449551d8
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-02-03 08:09:59 +0000

    Added timeout

--- core/src/main/java/org/bitcoinj/core/PeerSocketHandler.java
+++ core/src/main/java/org/bitcoinj/core/PeerSocketHandler.java
@@ -10,4 +10,52 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the s
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.core;
+
+import org.bitcoinj.net.AbstractTimeoutHandler;
+import org.bitcoinj.net.MessageWriteTarget;
+import org.bitcoinj.net.StreamConnection;
+import org.bitcoinj.utils.Threading;
+import com.google.common.annotations.VisibleForTesting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetSocketAddress;
+import java.nio.BufferUnderflowException;
+import java.nio.ByteBuffer;
+import java.nio.channels.NotYetConnectedException;
+import java.util.concurrent.locks.Lock;
+
+import static com.google.common.base.Preconditions.*;
+
+/**
+ * Handles high-level message (de)serialization for peers, acting as the bridge between the
+ * {@link org.bitcoinj.net} classes and {@link Peer}.
+ */
+public abstract class PeerSocketHandler extends AbstractTimeoutHandler implements StreamConnection {
+    private static final Logger log = LoggerFactory.getLogger(PeerSocketHandler.class);
+
+    private final MessageSerializer serializer;
+    protected PeerAddress peerAddress;
+    // If we close() before we know our writeTarget, set this to true to call writeTarget.closeConnection() right away.
+    private boolean closePending = false;
+    // writeTarget will be thread-safe, and may call into PeerGroup, which calls us, so we should call it unlocked
+    @VisibleForTesting protected MessageWriteTarget writeTarget = null;
+
+    // The ByteBuffers passed to us from the writeTarget are static in size, and usually smaller than some messages we
+    // will receive. For SPV clients, this should be rare (ie we're mostly dealing with small transactions), but for
+    // messages which are larger than the read buffer, we have to keep a temporary buffer with its bytes.
+    private byte[] largeReadBuffer;
+    private int largeReadBufferPos;
+    private BitcoinSerializer.BitcoinPacketHeader header;
+
+    private Lock lock = Threading.lock("PeerSocketHandler");
+
+    public PeerSocketHandler(Net
