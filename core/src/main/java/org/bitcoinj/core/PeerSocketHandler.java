5c2bb9f66319694f8647f5077c07a306c4ef17e6
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-02-03 20:52:43 +0000

    Temporary workaround for pester issue causing build to get stuck

--- core/src/main/java/org/bitcoinj/core/PeerSocketHandler.java
+++ core/src/main/java/org/bitcoinj/core/PeerSocketHandler.java
@@ -58,4 +58,69 @@ public abstract class PeerSocketHandler extends AbstractTimeoutHandler implement
 
     private Lock lock = Threading.lock("PeerSocketHandler");
 
-    public PeerSocketHandler(Net
+    public PeerSocketHandler(NetworkParameters params, InetSocketAddress remoteIp) {
+        checkNotNull(params);
+        serializer = params.getDefaultSerializer();
+        this.peerAddress = new PeerAddress(params, remoteIp);
+    }
+
+    public PeerSocketHandler(NetworkParameters params, PeerAddress peerAddress) {
+        checkNotNull(params);
+        serializer = params.getDefaultSerializer();
+        this.peerAddress = checkNotNull(peerAddress);
+    }
+
+    /**
+     * Sends the given message to the peer. Due to the asynchronousness of network programming, there is no guarantee
+     * the peer will have received it. Throws NotYetConnectedException if we are not yet connected to the remote peer.
+     * TODO: Maybe use something other than the unchecked NotYetConnectedException here
+     */
+    public void sendMessage(Message message) throws NotYetConnectedException {
+        lock.lock();
+        try {
+            if (writeTarget == null)
+                throw new NotYetConnectedException();
+        } finally {
+            lock.unlock();
+        }
+        // TODO: Some round-tripping could be avoided here
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            serializer.serialize(message, out);
+            writeTarget.writeBytes(out.toByteArray());
+        } catch (IOException e) {
+            exceptionCaught(e);
+        }
+    }
+
+    /**
+     * Closes the connection to the peer if one exists, or immediately closes the connection as soon as it opens
+     */
+    public void close() {
+        lock.lock();
+        try {
+            if (writeTarget == null) {
+                closePending = true;
+                return;
+            }
+        } finally {
+            lock.unlock();
+        }
+        writeTarget.closeConnection();
+    }
+
+    @Override
+    protected void timeoutOccurred() {
+        log.info("{}: Timed out", getAddress());
+        close();
+    }
+
+    /**
+     * Called every time a message is received from the network
+     */
+    protected abstract void processMessage(Message m) throws Exception;
+
+    @Override
+    public int receiveBytes(ByteBuffer buff) {
+        checkArgument(buff.position() == 0 &&
+                buff.capacity() >= BitcoinSerializer.BitcoinPa
