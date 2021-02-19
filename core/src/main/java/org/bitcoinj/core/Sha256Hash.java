352812d59723b94f8eaae1642fc08c07f8123a0c
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-02-19 08:33:21 +0000

    fixed #34

--- core/src/main/java/org/bitcoinj/core/Sha256Hash.java
+++ core/src/main/java/org/bitcoinj/core/Sha256Hash.java
@@ -18,4 +18,69 @@
 package org.bitcoinj.core;
 
 import com.google.common.io.ByteStreams;
-import com.google.common.primitiv
+import com.google.common.primitives.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * A Sha256Hash just wraps a byte[] so that equals and hashcode work correctly, allowing it to be used as keys in a
+ * map. It also checks that the length is correct and provides a bit more type safety.
+ */
+public class Sha256Hash implements Serializable, Comparable<Sha256Hash> {
+    public static final int LENGTH = 32; // bytes
+    public static final Sha256Hash ZERO_HASH = wrap(new byte[LENGTH]);
+
+    private final byte[] bytes;
+
+    /**
+     * Use {@link #wrap(byte[])} instead.
+     */
+    @Deprecated
+    public Sha256Hash(byte[] rawHashBytes) {
+        checkArgument(rawHashBytes.length == LENGTH);
+        this.bytes = rawHashBytes;
+    }
+
+    /**
+     * Use {@link #wrap(String)} instead.
+     */
+    @Deprecated
+    public Sha256Hash(String hexString) {
+        checkArgument(hexString.length() == LENGTH * 2);
+        this.bytes = Utils.HEX.decode(hexString);
+    }
+
+    /**
+     * Creates a new instance that wraps the given hash value.
+     *
+     * @param rawHashBytes the raw hash bytes to wrap
+     * @return a new instance
+     * @throws IllegalArgumentException if the given array length is not exactly 32
+     */
+    @SuppressWarnings("deprecation") // the constructor will be made private in the future
+    public static Sha256Hash wrap(byte[] rawHashBytes) {
+        return new Sha256Hash(rawHashBytes);
+    }
+
+    /**
+     * Creates a new instance that wraps the given hash value (represented as a hex string).
+     *
+     * @param hexString a hash value represented as a hex string
+     * @return a new instance
+     * @throws IllegalArgumentException if the given string is not a valid
+     *         hex string, or if it does not represent exactly 32 bytes
+     */
+    public static Sha256Hash wrap(String hexString) {
+        return wrap(Utils.HEX.decode(hexString));
+    }
+
+  
