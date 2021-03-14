fde57057d31f87ac4e8f2f54ba47a39a2222ec7d
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-03-14 11:47:14 +0000

    Fixed some bugs.

--- core/src/main/java/org/bitcoinj/core/VersionMessage.java
+++ core/src/main/java/org/bitcoinj/core/VersionMessage.java
@@ -7,4 +7,50 @@
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable la
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.core;
+
+import com.google.common.base.Objects;
+import com.google.common.net.InetAddresses;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Locale;
+
+/**
+ * <p>A VersionMessage holds information exchanged during connection setup with another peer. Most of the fields are not
+ * particularly interesting. The subVer field, since BIP 14, acts as a User-Agent string would. You can and should
+ * append to or change the subVer for your own software so other implementations can identify it, and you can look at
+ * the subVer field received from other nodes to see what they are running.</p>
+ *
+ * <p>After creating yourself a VersionMessage, you can pass it to {@link PeerGroup#setVersionMessage(VersionMessage)}
+ * to ensure it will be used for each new connection.</p>
+ *
+ * <p>Instances of this class are not safe for use by multiple threads.</p>
+ */
+public class VersionMessage extends Message {
+
+    /** The version of this library release, as a string. */
+    public static final String BITCOINJ_VERSION = "0.15-SNAPSHOT";
+    /** The value that is prepended to the subVer field of this application. */
+    public static final String LIBRARY_SUBVER = "/bitcoinj:" + BITCOINJ_VERSION + "/";
+
+    /** A services flag that denotes whether the peer has a copy of the block chain or not. */
+    public static final int NODE_NETWORK = 1 << 0;
+    /** A flag that denotes whether the peer supports the getutxos message or not. */
+    public static final int NODE_GETUTXOS = 1 << 1;
+    /** A service bit used by Bitcoin-ABC to announce Bitcoin Cash nodes. */
+    public static final int NODE_BITCOIN_CASH = 1 << 5;
+    /** A service bit used by BTC1 to announce Segwit2x nodes. */
+    public static final int NODE_SEGWIT2X = 1 << 7;
+    /** Indicates that a
