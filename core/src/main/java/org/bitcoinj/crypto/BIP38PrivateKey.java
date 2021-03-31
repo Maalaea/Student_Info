94e0ca349ecde71a2f5d03d7ff1df4689086e73c
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-03-31 18:37:17 +0000

    inotify: Fix erroneous update of bit count

--- core/src/main/java/org/bitcoinj/crypto/BIP38PrivateKey.java
+++ core/src/main/java/org/bitcoinj/crypto/BIP38PrivateKey.java
@@ -50,4 +50,48 @@ public class BIP38PrivateKey extends VersionedChecksummedBytes {
     public final byte[] content;
 
     public static final class BadPassphraseException extends Exception {
-    
+    }
+
+    /**
+     * Construct a password-protected private key from its Base58 representation.
+     * @param params
+     *            The network parameters of the chain that the key is for.
+     * @param base58
+     *            The textual form of the password-protected private key.
+     * @throws AddressFormatException
+     *             if the given base58 doesn't parse or the checksum is invalid
+     */
+    public static BIP38PrivateKey fromBase58(NetworkParameters params, String base58) throws AddressFormatException {
+        return new BIP38PrivateKey(params, base58);
+    }
+
+    /** @deprecated Use {@link #fromBase58(NetworkParameters, String)} */
+    @Deprecated
+    public BIP38PrivateKey(NetworkParameters params, String encoded) throws AddressFormatException {
+        super(encoded);
+        this.params = checkNotNull(params);
+        if (version != 0x01)
+            throw new AddressFormatException("Mismatched version number: " + version);
+        if (bytes.length != 38)
+            throw new AddressFormatException("Wrong number of bytes, excluding version byte: " + bytes.length);
+        hasLotAndSequence = (bytes[1] & 0x04) != 0; // bit 2
+        compressed = (bytes[1] & 0x20) != 0; // bit 5
+        if ((bytes[1] & 0x01) != 0) // bit 0
+            throw new AddressFormatException("Bit 0x01 reserved for future use.");
+        if ((bytes[1] & 0x02) != 0) // bit 1
+            throw new AddressFormatException("Bit 0x02 reserved for future use.");
+        if ((bytes[1] & 0x08) != 0) // bit 3
+            throw new AddressFormatException("Bit 0x08 reserved for future use.");
+        if ((bytes[1] & 0x10) != 0) // bit 4
+            throw new AddressFormatException("Bit 0x10 reserved for future use.");
+        final int byte0 = bytes[0] & 0xff;
+        if (byte0 == 0x42) {
+            // Non-EC-multiplied key
+            if ((bytes[1] & 0xc0) != 0xc0) // bits 6+7
+                throw new AddressFormatException("Bits 0x40 and 0x80 must be set for non-EC-multiplied keys.");
+            ecMultiply = false;
+            if (hasLotAndSequence)
+                throw new AddressFormatException("Non-EC-multiplied keys cannot have lot/sequence.");
+        } else if (byte0 == 0x43) {
+            // EC-multiplied key
+     
