b3c228a2c1ecf7a6357f6514751d2a01662a53f6
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-04-08 16:51:14 +0000

    Update translations

--- core/src/main/java/org/bitcoinj/crypto/DeterministicKey.java
+++ core/src/main/java/org/bitcoinj/crypto/DeterministicKey.java
@@ -2,4 +2,57 @@
  * Copyright 2013 Matija Mazi.
  * Copyright 2014 Andreas Schildbach
  *
- * Licensed under the Apache License, 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.crypto;
+
+import org.bitcoinj.core.*;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
+import com.google.common.collect.ImmutableList;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.math.ec.ECPoint;
+
+import javax.annotation.Nullable;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static org.bitcoinj.core.Utils.HEX;
+import static com.google.common.base.Preconditions.*;
+
+/**
+ * A deterministic key is a node in a {@link DeterministicHierarchy}. As per
+ * <a href="https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki">the BIP 32 specification</a> it is a pair
+ * (key, chaincode). If you know its path in the tree and its chain code you can derive more keys from this. To obtain
+ * one of these, you can call {@link HDKeyDerivation#createMasterPrivateKey(byte[])}.
+ */
+public class DeterministicKey extends ECKey {
+
+    /** Sorts deterministic keys in the order of their child number. That's <i>usually</i> the order used to derive them. */
+    public static final Comparator<ECKey> CHILDNUM_ORDER = new Comparator<ECKey>() {
+        @Override
+        public int compare(ECKey k1, ECKey k2) {
+            ChildNumber cn1 = ((DeterministicKey) k1).getChildNumber();
+            ChildNumber cn2 = ((DeterministicKey) k2).getChildNumber();
+            return cn1.compareTo(cn2);
+        }
+    };
+
+    private final DeterministicKey parent;
+    private final ImmutableList<ChildNumber> childNumberPath;
+    private final int depth;
+    private int parentFingerprint; // 0 if this key is root
