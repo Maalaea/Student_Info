dc902429ce62e0f87688710aa73d94a6c70f952c
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-04-19 19:19:07 +0000

    fix submodule

--- core/src/main/java/org/bitcoinj/crypto/HDKeyDerivation.java
+++ core/src/main/java/org/bitcoinj/crypto/HDKeyDerivation.java
@@ -25,4 +25,48 @@ import java.nio.*;
 import java.security.*;
 import java.util.*;
 
-i
+import static com.google.common.base.Preconditions.*;
+
+/**
+ * Implementation of the <a href="https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki">BIP 32</a>
+ * deterministic wallet child key generation algorithm.
+ */
+public final class HDKeyDerivation {
+    static {
+        // Init proper random number generator, as some old Android installations have bugs that make it unsecure.
+        if (Utils.isAndroidRuntime())
+            new LinuxSecureRandom();
+
+        RAND_INT = new BigInteger(256, new SecureRandom());
+    }
+
+    // Some arbitrary random number. Doesn't matter what it is.
+    private static final BigInteger RAND_INT;
+
+    private HDKeyDerivation() { }
+
+    /**
+     * Child derivation may fail (although with extremely low probability); in such case it is re-attempted.
+     * This is the maximum number of re-attempts (to avoid an infinite loop in case of bugs etc.).
+     */
+    public static final int MAX_CHILD_DERIVATION_ATTEMPTS = 100;
+
+    /**
+     * Generates a new deterministic key from the given seed, which can be any arbitrary byte array. However resist
+     * the temptation to use a string as the seed - any key derived from a password is likely to be weak and easily
+     * broken by attackers (this is not theoretical, people have had money stolen that way). This method checks
+     * that the given seed is at least 64 bits long.
+     *
+     * @throws HDDerivationException if generated master key is invalid (private key 0 or >= n).
+     * @throws IllegalArgumentException if the seed is less than 8 bytes and could be brute forced.
+     */
+    public static DeterministicKey createMasterPrivateKey(byte[] seed) throws HDDerivationException {
+        checkArgument(seed.length > 8, "Seed is too short and could be brute forced");
+        // Calculate I = HMAC-SHA512(key="Bitcoin seed", msg=S)
+        byte[] i = HDUtils.hmacSha512(HDUtils.createHmacSha512Digest("Bitcoin seed".getBytes()), seed);
+        // Split I into two 32-byte sequences, Il and Ir.
+        // Use Il as master secret key, and Ir as master chain code.
+        checkState(i.length == 64, i.length);
+        byte[] il = Arrays.copyOfRange(i, 0, 32);
+        byte[] ir = Arrays.copyOfRange(i, 32, 64);
+        Arrays.fill(
