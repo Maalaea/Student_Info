9cfe30f79628eb59d85f670357934d0ff62fb458
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-04-27 11:34:36 +0000

    [R]Started New Version

--- core/src/main/java/org/bitcoinj/crypto/KeyCrypterScrypt.java
+++ core/src/main/java/org/bitcoinj/crypto/KeyCrypterScrypt.java
@@ -12,4 +12,67 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the Licen
+ * limitations under the License.
+ */
+
+package org.bitcoinj.crypto;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Stopwatch;
+import com.google.protobuf.ByteString;
+import com.lambdaworks.crypto.SCrypt;
+import org.bitcoinj.core.Utils;
+import org.bitcoinj.wallet.Protos;
+import org.bitcoinj.wallet.Protos.ScryptParameters;
+import org.bitcoinj.wallet.Protos.Wallet.EncryptionType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.bouncycastle.crypto.BufferedBlockCipher;
+import org.bouncycastle.crypto.engines.AESFastEngine;
+import org.bouncycastle.crypto.modes.CBCBlockCipher;
+import org.bouncycastle.crypto.paddings.PaddedBufferedBlockCipher;
+import org.bouncycastle.crypto.params.KeyParameter;
+import org.bouncycastle.crypto.params.ParametersWithIV;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * <p>This class encrypts and decrypts byte arrays and strings using scrypt as the
+ * key derivation function and AES for the encryption.</p>
+ *
+ * <p>You can use this class to:</p>
+ *
+ * <p>1) Using a user password, create an AES key that can encrypt and decrypt your private keys.
+ * To convert the password to the AES key, scrypt is used. This is an algorithm resistant
+ * to brute force attacks. You can use the ScryptParameters to tune how difficult you
+ * want this to be generation to be.</p>
+ *
+ * <p>2) Using the AES Key generated above, you then can encrypt and decrypt any bytes using
+ * the AES symmetric cipher. Eight bytes of salt is used to prevent dictionary attacks.</p>
+ */
+public class KeyCrypterScrypt implements KeyCrypter {
+
+    private static final Logger log = LoggerFactory.getLogger(KeyCrypterScrypt.class);
+
+    /**
+     * Key length in bytes.
+     */
+    public static final int KEY_LENGTH = 32; // = 256 bits.
+
+    /**
+     * The size of an AES block in bytes.
+     * This is also the length of the initialisation vector.
+     */
+    public static final int BLOCK_LENGTH = 16;  // = 128 bits.
+
+    /**
+     * The length of the salt used.
+     */
+    public static final int SALT_LENGTH = 8;
+
+    static {
+        // Init proper random number generator, as some old Android installations have bugs that make it unsecure.
+        if (Utils.isAndroidR
