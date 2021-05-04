85cdab3e8115cfbcd37e6951b9c20a4802bc8bd6
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-05-04 14:42:31 +0000

    fix method

--- core/src/main/java/org/bitcoinj/crypto/MnemonicCode.java
+++ core/src/main/java/org/bitcoinj/crypto/MnemonicCode.java
@@ -37,4 +37,54 @@ import java.util.List;
 import static org.bitcoinj.core.Utils.HEX;
 
 /**
- * A MnemonicCode obj
+ * A MnemonicCode object may be used to convert between binary seed values and
+ * lists of words per <a href="https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki">the BIP 39
+ * specification</a>
+ */
+
+public class MnemonicCode {
+    private static final Logger log = LoggerFactory.getLogger(MnemonicCode.class);
+
+    private ArrayList<String> wordList;
+
+    private static final String BIP39_ENGLISH_RESOURCE_NAME = "mnemonic/wordlist/english.txt";
+    private static final String BIP39_ENGLISH_SHA256 = "ad90bf3beb7b0eb7e5acd74727dc0da96e0a280a258354e7293fb7e211ac03db";
+
+    /** UNIX time for when the BIP39 standard was finalised. This can be used as a default seed birthday. */
+    public static long BIP39_STANDARDISATION_TIME_SECS = 1381276800;
+
+    private static final int PBKDF2_ROUNDS = 2048;
+
+    public static MnemonicCode INSTANCE;
+
+    static {
+        try {
+            INSTANCE = new MnemonicCode();
+        } catch (FileNotFoundException e) {
+            // We expect failure on Android. The developer has to set INSTANCE themselves.
+            if (!Utils.isAndroidRuntime())
+                log.error("Could not find word list", e);
+        } catch (IOException e) {
+            log.error("Failed to load word list", e);
+        }
+    }
+
+    /** Initialise from the included word list. Won't work on Android. */
+    public MnemonicCode() throws IOException {
+        this(openDefaultWords(), BIP39_ENGLISH_SHA256);
+    }
+
+    private static InputStream openDefaultWords() throws IOException {
+        InputStream stream = MnemonicCode.class.getResourceAsStream(BIP39_ENGLISH_RESOURCE_NAME);
+        if (stream == null)
+            throw new FileNotFoundException(BIP39_ENGLISH_RESOURCE_NAME);
+        return stream;
+    }
+
+    /**
+     * Creates an MnemonicCode object, initializing with words read from the supplied input stream.  If a wordListDigest
+     * is supplied the digest of the words will be checked.
+     */
+    public MnemonicCode(InputStream wordstream, String wordListDigest) throws IOException, IllegalArgumentException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(wordstream, "UTF-8"));
+        wordList = new A
