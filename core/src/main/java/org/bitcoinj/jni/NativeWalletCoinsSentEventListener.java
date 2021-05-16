7cf075458ae3d7d6c8ffb0deb42cd30eaa8d6bc3
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-05-16 07:34:43 +0000

    Asdf

--- core/src/main/java/org/bitcoinj/jni/NativeWalletCoinsSentEventListener.java
+++ core/src/main/java/org/bitcoinj/jni/NativeWalletCoinsSentEventListener.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2013 Google Inc.
  *
@@ -17,16 +18,18 @@
 package org.bitcoinj.jni;
 
 import org.bitcoinj.wallet.Wallet;
-import org.bitcoinj.wallet.listeners.WalletChangeEventListener;
+import org.bitcoinj.wallet.listeners.WalletCoinsSentEventListener;
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.Transaction;
 
 /**
  * An event listener that relays events to a native C++ object. A pointer to that object is stored in
  * this class using JNI on the native side, thus several instances of this can point to different actual
  * native implementations.
  */
-public class NativeWalletChangeEventListener implements WalletChangeEventListener {
+public class NativeWalletCoinsSentEventListener implements WalletCoinsSentEventListener {
     public long ptr;
 
     @Override
-    public native void onWalletChanged(Wallet wallet);
+    public native void onCoinsSent(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance);
 }
--- core/src/main/java/org/bitcoinj/jni/NativeWalletEventListener.java
+++ core/src/main/java/org/bitcoinj/jni/NativeWalletEventListener.java
@@ -16,17 +16,25 @@
 
 package org.bitcoinj.jni;
 
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.script.Script;
 import org.bitcoinj.wallet.Wallet;
-import org.bitcoinj.wallet.listeners.WalletChangeEventListener;
+import org.bitcoinj.wallet.listeners.WalletEventListener;
+
+import java.util.List;
 
 /**
  * An event listener that relays events to a native C++ object. A pointer to that object is stored in
  * this class using JNI on the native side, thus several instances of this can point to different actual
  * native implementations.
  */
-public class NativeWalletChangeEventListener implements WalletChangeEventListener {
+public class NativeWalletEventListener implements WalletEventListener {
     public long ptr;
 
     @Override
-    public native void onWalletChanged(Wallet wallet);
-}
+    public native void onCoinsReceived(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance);
+
+    @Override
+    pub
