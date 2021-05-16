2d6ae9af9a825e2547ff445818924ab6ff4d79de
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-05-16 22:19:32 +0000

    Sloader update on _data/Bulma.json

--- core/src/main/java/org/bitcoinj/jni/NativeWalletReorganizeEventListener.java
+++ core/src/main/java/org/bitcoinj/jni/NativeWalletReorganizeEventListener.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright 2013 Google Inc.
  *
@@ -16,41 +17,17 @@
 
 package org.bitcoinj.jni;
 
-import org.bitcoinj.core.Coin;
-import org.bitcoinj.core.ECKey;
-import org.bitcoinj.core.Transaction;
-import org.bitcoinj.script.Script;
 import org.bitcoinj.wallet.Wallet;
-import org.bitcoinj.wallet.listeners.WalletEventListener;
-
-import java.util.List;
+import org.bitcoinj.wallet.listeners.WalletReorganizeEventListener;
 
 /**
  * An event listener that relays events to a native C++ object. A pointer to that object is stored in
  * this class using JNI on the native side, thus several instances of this can point to different actual
  * native implementations.
  */
-public class NativeWalletEventListener implements WalletEventListener {
+public class NativeWalletReorganizeEventListener implements WalletReorganizeEventListener {
     public long ptr;
 
     @Override
-    public native void onCoinsReceived(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance);
-
-    @Override
-    public native void onCoinsSent(Wallet wallet, Transaction tx, Coin prevBalance, Coin newBalance);
-
-    @Override
     public native void onReorganize(Wallet wallet);
-
-    @Override
-    public native void onTransactionConfidenceChanged(Wallet wallet, Transaction tx);
-
-    @Override
-    public native void onWalletChanged(Wallet wallet);
-
-    @Override
-    public native void onKeysAdded(List<ECKey> keys);
-
-    @Override
-    public native void onScriptsChanged(Wallet wallet, List<Script> scripts, boolean isAddingScripts);
 }
