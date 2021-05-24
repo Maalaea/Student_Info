1d8cc41eea5f978e88c5e020c993b7816a13b06f
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-05-24 16:52:00 +0000

    OAK-527: permissions (wip)

--- core/src/main/java/org/bitcoinj/params/MainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MainNetParams.java
@@ -29,4 +29,43 @@ import static com.google.common.base.Preconditions.*;
  */
 public class MainNetParams extends AbstractBitcoinNetParams {
     public static final int MAINNET_MAJORITY_WINDOW = 1000;
- 
+    public static final int MAINNET_MAJORITY_REJECT_BLOCK_OUTDATED = 950;
+    public static final int MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE = 750;
+
+    public MainNetParams() {
+        super();
+        interval = INTERVAL;
+        targetTimespan = TARGET_TIMESPAN;
+        maxTarget = Utils.decodeCompactBits(0x1d00ffffL);
+        dumpedPrivateKeyHeader = 128;
+        addressHeader = 0;
+        p2shHeader = 5;
+        p2wpkhHeader = 6;
+        p2wshHeader = 10;
+        acceptableAddressCodes = new int[] { addressHeader, p2shHeader, p2wpkhHeader, p2wshHeader };
+        port = 8333;
+        packetMagic = 0xf9beb4d9L;
+        bip32HeaderPub = 0x0488B21E; //The 4 byte header that serializes in base58 to "xpub".
+        bip32HeaderPriv = 0x0488ADE4; //The 4 byte header that serializes in base58 to "xprv"
+        bip49HeaderPub = 0x049D7CB2; //The 4 byte header that serializes in base58 to "ypub".
+        bip49HeaderPriv = 0x049D7878; //The 4 byte header that serializes in base58 to "yprv"
+        bip84HeaderPub = 0x04B24746; //The 4 byte header that serializes in base58 to "zpub".
+        bip84HeaderPriv = 0x04B2430C; //The 4 byte header that serializes in base58 to "zprv"
+
+        majorityEnforceBlockUpgrade = MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE;
+        majorityRejectBlockOutdated = MAINNET_MAJORITY_REJECT_BLOCK_OUTDATED;
+        majorityWindow = MAINNET_MAJORITY_WINDOW;
+
+        genesisBlock.setDifficultyTarget(0x1d00ffffL);
+        genesisBlock.setTime(1231006505L);
+        genesisBlock.setNonce(2083236893);
+        id = ID_MAINNET;
+        subsidyDecreaseBlockCount = 210000;
+        spendableCoinbaseDepth = 100;
+        String genesisHash = genesisBlock.getHashAsString();
+        checkState(genesisHash.equals("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
+                genesisHash);
+
+        // This contains (at a minimum) the blocks which are not BIP30 compliant. BIP30 changed how duplicate
+        // transactions are handled. Duplicated transactions could occur in the case where a coinbase had the same
+        // extraNonce and the same outputs but appeared at different heights, and greatly complicated re-org handlin
