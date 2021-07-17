8f14e3f81fb775d9131c77b732e4341d7e1b95d1
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-07-17 07:05:20 +0000

    Pulled from master

--- core/src/main/java/org/bitcoinj/protocols/channels/PaymentChannelClientState.java
+++ core/src/main/java/org/bitcoinj/protocols/channels/PaymentChannelClientState.java
@@ -16,4 +16,42 @@
 
 package org.bitcoinj.protocols.channels;
 
-import com.google.common.annotations
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Throwables;
+import com.google.common.collect.Multimap;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import org.bitcoinj.core.*;
+import org.bitcoinj.crypto.TransactionSignature;
+import org.bitcoinj.protocols.channels.IPaymentChannelClient.ClientChannelProperties;
+import org.bitcoinj.script.Script;
+import org.bitcoinj.utils.Threading;
+import org.bitcoinj.wallet.Wallet;
+import org.bitcoinj.wallet.listeners.WalletCoinsReceivedEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.bouncycastle.crypto.params.KeyParameter;
+
+import javax.annotation.Nullable;
+
+import static com.google.common.base.Preconditions.*;
+
+/**
+ * <p>A payment channel is a method of sending money to someone such that the amount of money you send can be adjusted
+ * after the fact, in an efficient manner that does not require broadcasting to the network. This can be used to
+ * implement micropayments or other payment schemes in which immediate settlement is not required, but zero trust
+ * negotiation is. Note that this class only allows the amount of money sent to be incremented, not decremented.</p>
+ *
+ * <p>This class has two subclasses, {@link PaymentChannelV1ClientState} and {@link PaymentChannelV2ClientState} for
+ * protocols version 1 and 2.</p>
+ *
+ * <p>This class implements the core state machine for the client side of the protocol. The server side is implemented
+ * by {@link PaymentChannelServerState} and {@link PaymentChannelClientConnection} implements a network protocol
+ * suitable for TCP/IP connections which moves this class through each state. We say that the party who is sending funds
+ * is the <i>client</i> or <i>initiating party</i>. The party that is receiving the funds is the <i>server</i> or
+ * <i>receiving party</i>. Although the underlying Bitcoin protocol is capable of more complex relationships than that,
+ * this class implements only the simplest case.</p>
+ *
+ * <p>A channel has an expiry parameter. If the server halts after the multi-signature contract which locks
+ * up the given value is broadcast you 
