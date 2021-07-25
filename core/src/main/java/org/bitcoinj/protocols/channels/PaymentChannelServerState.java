ab679bf1181f207d82298c0f3394c62f3b6ed27d
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-07-25 14:27:50 +0000

    Update squid.conf

--- core/src/main/java/org/bitcoinj/protocols/channels/PaymentChannelServerState.java
+++ core/src/main/java/org/bitcoinj/protocols/channels/PaymentChannelServerState.java
@@ -46,4 +46,26 @@ import static com.google.common.base.Preconditions.checkState;
  * implement micropayments or other payment schemes in which immediate settlement is not required, but zero trust
  * negotiation is. Note that this class only allows the amount of money received to be incremented, not decremented.</p>
  *
- * <p>There are two subclasses that 
+ * <p>There are two subclasses that implement this one, for versions 1 and 2 of the protocol -
+ * {@link PaymentChannelV1ServerState} and {@link PaymentChannelV2ServerState}.</p>
+ *
+ * <p>This class implements the core state machine for the server side of the protocol. The client side is implemented
+ * by {@link PaymentChannelV1ClientState} and {@link PaymentChannelServerListener} implements the server-side network
+ * protocol listening for TCP/IP connections and moving this class through each state. We say that the party who is
+ * sending funds is the <i>client</i> or <i>initiating party</i>. The party that is receiving the funds is the
+ * <i>server</i> or <i>receiving party</i>. Although the underlying Bitcoin protocol is capable of more complex
+ * relationships than that, this class implements only the simplest case.</p>
+ *
+ * <p>To protect clients from malicious servers, a channel has an expiry parameter. When this expiration is reached, the
+ * client will broadcast the created refund  transaction and take back all the money in this channel. Because this is
+ * specified in terms of block timestamps, it is fairly fuzzy and it is possible to spend the refund transaction up to a
+ * few hours before the actual timestamp. Thus, it is very important that the channel be closed with plenty of time left
+ * to get the highest value payment transaction confirmed before the expire time (minimum 3-4 hours is suggested if the
+ * payment transaction has enough fee to be confirmed in the next block or two).</p>
+ *
+ * <p>To begin, we must provide the client with a pubkey which we wish to use for the multi-sig contract which locks in
+ * the channel. The client will then provide us with an incomplete refund transaction and the pubkey which they used in
+ * the multi-sig contract. We use this pubkey to recreate the multi-sig output and then sign that to the refund
+ * transaction. We provide that signature to the client and they then have the ability to spend the refund transaction
+ * at the specified expire time. The client then provides us with the full, signed multi-sig contract which we verify
+ * and broadcast, locking in their funds until we spend a payment transaction or the expir
