54521a06e9d0a2f137fa5d9f6f469927a52ca4c1
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-08-10 05:37:39 +0000

    update gemfiles

--- core/src/main/java/org/bitcoinj/protocols/channels/PaymentChannelV1ServerState.java
+++ core/src/main/java/org/bitcoinj/protocols/channels/PaymentChannelV1ServerState.java
@@ -2,4 +2,59 @@
  * Copyright 2013 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compl
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.protocols.channels;
+
+import com.google.common.collect.*;
+import org.bitcoinj.core.*;
+import org.bitcoinj.crypto.TransactionSignature;
+import org.bitcoinj.script.Script;
+import org.bitcoinj.script.ScriptBuilder;
+import org.bitcoinj.wallet.SendRequest;
+import org.bitcoinj.wallet.Wallet;
+
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.SettableFuture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.bouncycastle.crypto.params.KeyParameter;
+
+import javax.annotation.Nullable;
+import java.util.Locale;
+
+import static com.google.common.base.Preconditions.*;
+
+/**
+ * Version 1 of the payment channel server state object. Common functionality is
+ * present in the parent class.
+ */
+public class PaymentChannelV1ServerState extends PaymentChannelServerState {
+    private static final Logger log = LoggerFactory.getLogger(PaymentChannelV1ServerState.class);
+
+    // The total value locked into the multi-sig output and the value to us in the last signature the client provided
+    private Coin feePaidForPayment;
+
+    // The client key for the multi-sig contract
+    // We currently also use the serverKey for payouts, but this is not required
+    protected ECKey clientKey;
+
+    // The refund/change transaction output that goes back to the client
+    private TransactionOutput clientOutput;
+    private long refundTransactionUnlockTimeSecs;
+
+    PaymentChannelV1ServerState(StoredServerChannel storedServerChannel, Wallet wallet, TransactionBroadcaster broadcaster) throws VerificationException {
+        super(storedServerChannel, wallet, broadcaster);
+        synchronized (storedServerCh
