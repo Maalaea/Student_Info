c2f2b9a0a32a04e05efd230450a623d5ee4d5aa7
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-08-19 10:09:45 +0000

    Bump src/code.cloudfoundry.org/gorouter

--- core/src/main/java/org/bitcoinj/protocols/channels/StoredPaymentChannelClientStates.java
+++ core/src/main/java/org/bitcoinj/protocols/channels/StoredPaymentChannelClientStates.java
@@ -33,4 +33,49 @@ import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.util.*;
-imp
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
+/**
+ * This class maintains a set of {@link StoredClientChannel}s, automatically (re)broadcasting the contract transaction
+ * and broadcasting the refund transaction over the given {@link TransactionBroadcaster}.
+ */
+public class StoredPaymentChannelClientStates implements WalletExtension {
+    private static final Logger log = LoggerFactory.getLogger(StoredPaymentChannelClientStates.class);
+    static final String EXTENSION_ID = StoredPaymentChannelClientStates.class.getName();
+    static final int MAX_SECONDS_TO_WAIT_FOR_BROADCASTER_TO_BE_SET = 10;
+
+    @GuardedBy("lock") @VisibleForTesting final HashMultimap<Sha256Hash, StoredClientChannel> mapChannels = HashMultimap.create();
+    @VisibleForTesting final Timer channelTimeoutHandler = new Timer(true);
+
+    private Wallet containingWallet;
+    private final SettableFuture<TransactionBroadcaster> announcePeerGroupFuture = SettableFuture.create();
+
+    protected final ReentrantLock lock = Threading.lock("StoredPaymentChannelClientStates");
+
+    /**
+     * Creates a new StoredPaymentChannelClientStates and associates it with the given {@link Wallet} and
+     * {@link TransactionBroadcaster} which are used to complete and announce contract and refund
+     * transactions.
+     */
+    public StoredPaymentChannelClientStates(@Nullable Wallet containingWallet, TransactionBroadcaster announcePeerGroup) {
+        setTransactionBroadcaster(announcePeerGroup);
+        this.containingWallet = containingWallet;
+    }
+
+    /**
+     * Creates a new StoredPaymentChannelClientStates and associates it with the given {@link Wallet}
+     *
+     * Use this constructor if you use WalletAppKit, it will provide the broadcaster for you (no need to call the setter)
+     */
+    public StoredPaymentChannelClientStates(@Nullable Wallet containingWallet) {
+        this.containingWallet = containingWallet;
+    }
+
+    /**
+     * Use this setter if the broadcaste
