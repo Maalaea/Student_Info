ac207e90e903218d0f0ae2f7421a1624916599a2
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-10-15 13:31:51 +0000

    Run scan-local.sh on gcc:...

--- core/src/main/java/org/bitcoinj/signers/LocalTransactionSigner.java
+++ core/src/main/java/org/bitcoinj/signers/LocalTransactionSigner.java
@@ -28,4 +28,46 @@ import org.bitcoinj.script.Script.VerifyFlag;
 import org.bitcoinj.wallet.KeyBag;
 import org.bitcoinj.wallet.RedeemData;
 import org.slf4j.Logger;
-impo
+import org.slf4j.LoggerFactory;
+
+/**
+ * <p>{@link TransactionSigner} implementation for signing inputs using keys from provided {@link org.bitcoinj.wallet.KeyBag}.</p>
+ * <p>This signer doesn't create input scripts for tx inputs. Instead it expects inputs to contain scripts with
+ * empty sigs and replaces one of the empty sigs with calculated signature.
+ * </p>
+ * <p>This signer is always implicitly added into every wallet and it is the first signer to be executed during tx
+ * completion. As the first signer to create a signature, it stores derivation path of the signing key in a given
+ * {@link ProposedTransaction} object that will be also passed then to the next signer in chain. This allows other
+ * signers to use correct signing key for P2SH inputs, because all the keys involved in a single P2SH address have
+ * the same derivation path.</p>
+ * <p>This signer always uses {@link org.bitcoinj.core.Transaction.SigHash#ALL} signing mode.</p>
+ */
+public class LocalTransactionSigner extends StatelessTransactionSigner {
+    private static final Logger log = LoggerFactory.getLogger(LocalTransactionSigner.class);
+
+    /**
+     * Verify flags that are safe to use when testing if an input is already
+     * signed.
+     */
+    private static final EnumSet<VerifyFlag> MINIMUM_VERIFY_FLAGS = EnumSet.of(VerifyFlag.P2SH,
+        VerifyFlag.NULLDUMMY);
+
+    @Override
+    public boolean isReady() {
+        return true;
+    }
+
+    @Override
+    public boolean signInputs(ProposedTransaction propTx, KeyBag keyBag) {
+        Transaction tx = propTx.partialTx;
+        int numInputs = tx.getInputs().size();
+        for (int i = 0; i < numInputs; i++) {
+            TransactionInput txIn = tx.getInput(i);
+            if (txIn.getConnectedOutput() == null) {
+                log.warn("Missing connected output, assuming input {} is already signed.", i);
+                continue;
+            }
+
+            try {
+                // We assume if its already signed, its hopefully got a SIGHASH type that will not invalidate when
+                // we sign missing pieces (to check this would require either assuming any sign
