e06667ab5c599adfa498939abbab2c478a1f7239
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-10-23 12:41:07 +0000

    fixing namespace

--- core/src/main/java/org/bitcoinj/uri/RequiredFieldValidationException.java
+++ core/src/main/java/org/bitcoinj/uri/RequiredFieldValidationException.java
@@ -1,3 +1,4 @@
+
 /*
  * Copyright by the original author or authors.
  * 
@@ -17,23 +18,24 @@
 package org.bitcoinj.uri;
 
 /**
- * <p>Exception to provide the following to {@link org.bitcoinj.uri.BitcoinURI}:</p>
+ * <p>Exception to provide the following to {@link BitcoinURI}:</p>
  * <ul>
  * <li>Provision of parsing error messages</li>
  * </ul>
- * <p>This exception occurs when an optional field is detected (under the Bitcoin URI scheme) and fails
- * to pass the associated test (such as {@code amount} not being a valid number).</p>
+ * <p>This exception occurs when a required field is detected (under the BIP21 rules) and fails
+ * to pass the associated test (such as {@code req-expires} being out of date), or the required field is unknown
+ * to this version of the client in which case it should fail for security reasons.</p>
  *
  * @since 0.3.0
  *         
  */
-public class OptionalFieldValidationException extends BitcoinURIParseException {
+public class RequiredFieldValidationException extends BitcoinURIParseException {
 
-    public OptionalFieldValidationException(String s) {
+    public RequiredFieldValidationException(String s) {
         super(s);
     }
 
-    public OptionalFieldValidationException(String s, Throwable throwable) {
+    public RequiredFieldValidationException(String s, Throwable throwable) {
         super(s, throwable);
     }
 }
