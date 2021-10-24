2d14c27d9a99e08c600d997927cfa1d51f04872b
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-10-24 13:21:51 +0000

    - [ZBX-954] minor spacing fixes

--- core/src/main/java/org/bitcoinj/utils/BaseTaggableObject.java
+++ core/src/main/java/org/bitcoinj/utils/BaseTaggableObject.java
@@ -7,4 +7,66 @@
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
  *
- * Un
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.utils;
+
+import com.google.common.collect.Maps;
+import com.google.protobuf.ByteString;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * A simple implementation of {@link TaggableObject} that just uses a lazily created hashmap that is
+ * synchronized on this objects Java monitor.
+ */
+public class BaseTaggableObject implements TaggableObject {
+    @Nullable protected Map<String, ByteString> tags;
+
+    /** {@inheritDoc} */
+    @Override
+    @Nullable
+    public synchronized ByteString maybeGetTag(String tag) {
+        if (tags == null)
+            return null;
+        else
+            return tags.get(tag);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public ByteString getTag(String tag) {
+        ByteString b = maybeGetTag(tag);
+        if (b == null)
+            throw new IllegalArgumentException("Unknown tag " + tag);
+        return b;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public synchronized void setTag(String tag, ByteString value) {
+        checkNotNull(tag);
+        checkNotNull(value);
+        if (tags == null)
+            tags = new HashMap<>();
+        tags.put(tag, value);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public synchronized Map<String, ByteString> getTags() {
+        if (tags != null)
+            return Maps.newHashMap(tags);
+        else
+            return Maps.newHashMap();
+    }
+}
