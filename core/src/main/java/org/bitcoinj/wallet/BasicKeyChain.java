ec900e6a75d75dad84ef68202b229c5911045fd8
Maalaea <110383916+Maalaea@users.noreply.github.com>
2021-11-03 09:20:11 +0000

    Updated demo project

--- core/src/main/java/org/bitcoinj/wallet/BasicKeyChain.java
+++ core/src/main/java/org/bitcoinj/wallet/BasicKeyChain.java
@@ -36,3 +36,67 @@ import java.util.concurrent.locks.ReentrantLock;
 
 import static com.google.common.base.Preconditions.*;
 
+/**
+ * A {@link KeyChain} that implements the simplest model possible: it can have keys imported into it, and just acts as
+ * a dumb bag of keys. It will, left to its own devices, always return the same key for usage by the wallet, although
+ * it will automatically add one to itself if it's empty or if encryption is requested.
+ */
+public class BasicKeyChain implements EncryptableKeyChain {
+    private final ReentrantLock lock = Threading.lock("BasicKeyChain");
+
+    // Maps used to let us quickly look up a key given data we find in transcations or the block chain.
+    private final LinkedHashMap<ByteString, ECKey> hashToKeys;
+    private final LinkedHashMap<ByteString, ECKey> pubkeyToKeys;
+    @Nullable private final KeyCrypter keyCrypter;
+    private boolean isWatching;
+
+    private final CopyOnWriteArrayList<ListenerRegistration<KeyChainEventListener>> listeners;
+
+    public BasicKeyChain() {
+        this(null);
+    }
+
+    public BasicKeyChain(@Nullable KeyCrypter crypter) {
+        this.keyCrypter = crypter;
+        hashToKeys = new LinkedHashMap<>();
+        pubkeyToKeys = new LinkedHashMap<>();
+        listeners = new CopyOnWriteArrayList<>();
+    }
+
+    /** Returns the {@link KeyCrypter} in use or null if the key chain is not encrypted. */
+    @Override
+    @Nullable
+    public KeyCrypter getKeyCrypter() {
+        lock.lock();
+        try {
+            return keyCrypter;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public ECKey getKey(@Nullable KeyPurpose ignored) {
+        lock.lock();
+        try {
+            if (hashToKeys.isEmpty()) {
+                checkState(keyCrypter == null);   // We will refuse to encrypt an empty key chain.
+                final ECKey key = new ECKey();
+                importKeyLocked(key);
+                queueOnKeysAdded(ImmutableList.of(key));
+            }
+            return hashToKeys.values().iterator().next();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public List<ECKey> getKeys(@Nullable KeyPurpose purpose, int numberOfKeys) {
+        checkArgument(numberOfKeys > 0);
+        lock.lock();
+        try {
+            if (hashToKeys.size() < numberOfKeys) {
+                checkState(keyCrypter == null);
+
+            
