8604c10838c7e665b0fb18dd77257e31aa2d0e0d
Maalaea <110383916+Maalaea@users.noreply.github.com>
2022-01-04 22:32:47 +0000

    add a readme file

--- core/src/main/java/org/bitcoinj/wallet/WalletProtobufSerializer.java
+++ core/src/main/java/org/bitcoinj/wallet/WalletProtobufSerializer.java
@@ -18,4 +18,49 @@
 package org.bitcoinj.wallet;
 
 import org.bitcoinj.core.*;
-import or
+import org.bitcoinj.core.TransactionConfidence.ConfidenceType;
+import org.bitcoinj.crypto.KeyCrypter;
+import org.bitcoinj.crypto.KeyCrypterScrypt;
+import org.bitcoinj.script.Script;
+import org.bitcoinj.script.ScriptException;
+import org.bitcoinj.signers.LocalTransactionSigner;
+import org.bitcoinj.signers.TransactionSigner;
+import org.bitcoinj.utils.ExchangeRate;
+import org.bitcoinj.utils.Fiat;
+import org.bitcoinj.wallet.Protos.Wallet.EncryptionType;
+
+import com.google.common.collect.Lists;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.CodedInputStream;
+import com.google.protobuf.CodedOutputStream;
+import com.google.protobuf.TextFormat;
+import com.google.protobuf.WireFormat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.*;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * Serialize and de-serialize a wallet to a byte stream containing a
+ * <a href="https://developers.google.com/protocol-buffers/docs/overview">protocol buffer</a>. Protocol buffers are
+ * a data interchange format developed by Google with an efficient binary representation, a type safe specification
+ * language and compilers that generate code to work with those data structures for many languages. Protocol buffers
+ * can have their format evolved over time: conceptually they represent data using (tag, length, value) tuples. The
+ * format is defined by the <tt>wallet.proto</tt> file in the bitcoinj source distribution.<p>
+ *
+ * This class is used through its static methods. The most common operations are writeWallet and readWallet, which do
+ * the obvious operations on Output/InputStreams. You can use a {@link java.io.ByteArrayInputStream} and equivalent
+ * {@link java.io.ByteArrayOutputStream} if you'd like byte arrays instead. The protocol buffer can also be manipulated
+ * in its object form if you'd like to modify the flattened data structure before serialization to binary.<p>
+ *
+ * You can extend the wallet format with additional fields sp
