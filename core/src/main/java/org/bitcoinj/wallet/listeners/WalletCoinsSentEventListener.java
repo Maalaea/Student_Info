a238381f69520c8acd4cb661aef0934d60f7b506
Maalaea <110383916+Maalaea@users.noreply.github.com>
2022-01-19 07:32:13 +0000

    Tweaks and fixes

---
+++ core/src/main/java/org/bitcoinj/wallet/listeners/WalletCoinsSentEventListener.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.wallet.listeners;
+
+import org.bitcoinj.core.Coin;
+import org.bitcoinj.core.Transaction;
+import org.bitcoinj.wallet.Wallet;
+
+/**
+ * <p>Implementors are called when the contents of the wallet changes, for instance due to receiving/sending money
+ * or a block chain re-organize. It may be convenient to derive from {@link AbstractWalletEventListener} instead.</p>
+ */
+public interface WalletCoinsSentEventListener {
+
+    /**
+     * This is called when a transaction is seen that sends coins <b>from</b> this wallet, either
+     * because it was broadcast across the network or because a block was received. This may at first glance seem
+     * useless, because in the common case you already know about such transactions because you created them with
+     * the Wallets createSend/sendCoins methods. However when you have a wallet containing only keys, and you wish
+     * to replay the block chain to fill it with transactions, it's useful to find out when a transaction is discovered
+     * that sends coins from th
