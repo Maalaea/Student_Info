99fbb3dc35cd0c8a3b0fda919286af14aea7990e
Maalaea <110383916+Maalaea@users.noreply.github.com>
2022-02-27 20:24:19 +0000

    Update pythonapp.yml

--- core/src/test/java/org/bitcoinj/core/BitcoindComparisonTool.java
+++ core/src/test/java/org/bitcoinj/core/BitcoindComparisonTool.java
@@ -15,4 +15,57 @@
  * limitations under the License.
  */
 
-packag
+package org.bitcoinj.core;
+
+import com.google.common.base.*;
+import com.google.common.collect.*;
+import com.google.common.util.concurrent.*;
+import org.bitcoinj.core.listeners.*;
+import org.bitcoinj.net.*;
+import org.bitcoinj.params.*;
+import org.bitcoinj.store.*;
+import org.bitcoinj.utils.*;
+import org.slf4j.*;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.atomic.*;
+
+/**
+ * A tool for comparing the blocks which are accepted/rejected by bitcoind/bitcoinj
+ * It is designed to run as a testnet-in-a-box network between a single bitcoind node and bitcoinj
+ * It is not an automated unit-test because it requires a bit more set-up...read comments below
+ */
+public class BitcoindComparisonTool {
+    private static final Logger log = LoggerFactory.getLogger(BitcoindComparisonTool.class);
+
+    private static NetworkParameters params;
+    private static FullPrunedBlockChain chain;
+    private static Sha256Hash bitcoindChainHead;
+    private static volatile InventoryMessage mostRecentInv = null;
+
+    static class BlockWrapper {
+        public Block block;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BriefLogFormatter.init();
+        System.out.println("USAGE: bitcoinjBlockStoreLocation runExpensiveTests(1/0) [port=18444]");
+        boolean runExpensiveTests = args.length > 1 && Integer.parseInt(args[1]) == 1;
+
+        params = RegTestParams.get();
+        Context ctx = new Context(params);
+
+        File blockFile = File.createTempFile("testBlocks", ".dat");
+        blockFile.deleteOnExit();
+
+        FullBlockTestGenerator generator = new FullBlockTestGenerator(params);
+        final RuleList blockList = generator.getBlocksToTest(false, runExpensiveTests, blockFile);
+        final Map<Sha256Hash, Block> preloadedBlocks = new HashMap<>();
+        final Iterator<Block> blocks = new BlockFileLoader(params, Arrays.asList(blockFile));
+
+        try {
+            H2FullPrunedBlockStore store = new H2FullPrunedBlockStore(params, args.length > 0 ? args[0] : "BitcoindComparisonTool", blockList.maximumReorgBlockCount);
+            store.resetStore();
+            //store = new MemoryFullPrunedBlockStore(pa
