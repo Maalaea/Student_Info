50766920f93c53eb366a24cdac7730ea11a3de9c
Maalaea <110383916+Maalaea@users.noreply.github.com>
2022-03-11 06:11:17 +0000

    Added missing package

--- core/src/test/java/org/bitcoinj/core/BlockChainTest.java
+++ core/src/test/java/org/bitcoinj/core/BlockChainTest.java
@@ -11,4 +11,68 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for th
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.core;
+
+import org.bitcoinj.params.MainNetParams;
+import org.bitcoinj.params.TestNet2Params;
+import org.bitcoinj.params.UnitTestParams;
+import org.bitcoinj.store.BlockStore;
+import org.bitcoinj.store.MemoryBlockStore;
+import org.bitcoinj.testing.FakeTxBuilder;
+import org.bitcoinj.utils.BriefLogFormatter;
+import org.bitcoinj.wallet.Wallet;
+import org.bitcoinj.wallet.Wallet.BalanceType;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import org.junit.rules.ExpectedException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import static org.bitcoinj.core.Coin.*;
+import static org.bitcoinj.testing.FakeTxBuilder.createFakeBlock;
+import static org.bitcoinj.testing.FakeTxBuilder.createFakeTx;
+import static org.junit.Assert.*;
+
+// Handling of chain splits/reorgs are in ChainSplitTests.
+
+public class BlockChainTest {
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private BlockChain testNetChain;
+
+    private Wallet wallet;
+    private BlockChain chain;
+    private BlockStore blockStore;
+    private Address coinbaseTo;
+    private static final NetworkParameters PARAMS = UnitTestParams.get();
+    private final StoredBlock[] block = new StoredBlock[1];
+    private Transaction coinbaseTransaction;
+
+    private static class TweakableTestNet2Params extends TestNet2Params {
+        public void setMaxTarget(BigInteger limit) {
+            maxTarget = limit;
+        }
+    }
+    private static final TweakableTestNet2Params testNet = new TweakableTestNet2Params();
+
+    private void resetBlockStore() {
+        blockStore = new MemoryBlockStore(PARAMS);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        BriefLogFormatter.initVerbose();
+        Context.propagate(new Context(testNet, 100, Coin.ZERO, false));
+        testNetChain = new BlockChain(testNet, new Wallet(testNet), new MemoryBlockStore(testNet));
+        Context.propagate(new
