181c53c851b8fff8e4abfbaa7df77b17985d36ab
Maalaea <110383916+Maalaea@users.noreply.github.com>
2022-04-14 06:31:11 +0000

    Remove unused files

--- core/src/test/java/org/bitcoinj/core/CoinTest.java
+++ core/src/test/java/org/bitcoinj/core/CoinTest.java
@@ -67,4 +67,70 @@ public class CoinTest {
         assertEquals(MAX_MONEY.negate(), valueOf(MAX_MONEY.value * -1));
         valueOf(MAX_MONEY.value + 1);
         valueOf((MAX_MONEY.value * -1) - 1);
-        valueOf(Long.M
+        valueOf(Long.MAX_VALUE);
+        valueOf(Long.MIN_VALUE);
+
+        try {
+            valueOf(1, -1);
+            fail();
+        } catch (IllegalArgumentException e) {}
+        try {
+            valueOf(-1, 0);
+            fail();
+        } catch (IllegalArgumentException e) {}
+    }
+
+    @Test
+    public void testOperators() {
+        assertTrue(SATOSHI.isPositive());
+        assertFalse(SATOSHI.isNegative());
+        assertFalse(SATOSHI.isZero());
+        assertFalse(NEGATIVE_SATOSHI.isPositive());
+        assertTrue(NEGATIVE_SATOSHI.isNegative());
+        assertFalse(NEGATIVE_SATOSHI.isZero());
+        assertFalse(ZERO.isPositive());
+        assertFalse(ZERO.isNegative());
+        assertTrue(ZERO.isZero());
+
+        assertTrue(valueOf(2).isGreaterThan(valueOf(1)));
+        assertFalse(valueOf(2).isGreaterThan(valueOf(2)));
+        assertFalse(valueOf(1).isGreaterThan(valueOf(2)));
+        assertTrue(valueOf(1).isLessThan(valueOf(2)));
+        assertFalse(valueOf(2).isLessThan(valueOf(2)));
+        assertFalse(valueOf(2).isLessThan(valueOf(1)));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testMultiplicationOverflow() {
+        Coin.valueOf(Long.MAX_VALUE).multiply(2);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testMultiplicationUnderflow() {
+        Coin.valueOf(Long.MIN_VALUE).multiply(2);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testAdditionOverflow() {
+        Coin.valueOf(Long.MAX_VALUE).add(Coin.SATOSHI);
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testSubstractionUnderflow() {
+        Coin.valueOf(Long.MIN_VALUE).subtract(Coin.SATOSHI);
+    }
+
+    @Test
+    public void testToFriendlyString() {
+        assertEquals("1.00 BTC", COIN.toFriendlyString());
+        assertEquals("1.23 BTC", valueOf(1, 23).toFriendlyString());
+        assertEquals("0.001 BTC", COIN.divide(1000).toFriendlyString());
+        assertEquals("-1.23 BTC", valueOf(1, 23).negate().toFriendlyString());
+    }
+
+    /**
+     * Test the bitcoinValueToPlainString amount formatter
+     */
+    @Test
+    public void testToPlainString() {
+        assertEquals("0.0015", Coin
