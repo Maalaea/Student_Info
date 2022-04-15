40205f93e4ae232023f533548c91c4d104e419b8
Maalaea <110383916+Maalaea@users.noreply.github.com>
2022-04-15 15:24:20 +0000

    facilities

--- core/src/test/java/org/bitcoinj/core/FilteredBlockAndPartialMerkleTreeTests.java
+++ core/src/test/java/org/bitcoinj/core/FilteredBlockAndPartialMerkleTreeTests.java
@@ -7,3 +7,53 @@
  * You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.core;
+
+import com.google.common.collect.*;
+import org.bitcoinj.core.TransactionConfidence.*;
+import org.bitcoinj.store.*;
+import org.bitcoinj.testing.*;
+import org.bitcoinj.wallet.*;
+import org.junit.*;
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+import java.math.*;
+import java.util.*;
+
+import static org.bitcoinj.core.Utils.*;
+import static org.junit.Assert.*;
+
+@RunWith(value = Parameterized.class)
+public class FilteredBlockAndPartialMerkleTreeTests extends TestWithPeerGroup {
+    @Parameterized.Parameters
+    public static Collection<ClientType[]> parameters() {
+        return Arrays.asList(new ClientType[] {ClientType.NIO_CLIENT_MANAGER},
+                             new ClientType[] {ClientType.BLOCKING_CLIENT_MANAGER});
+    }
+
+    public FilteredBlockAndPartialMerkleTreeTests(ClientType clientType) {
+        super(clientType);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        context = new Context(PARAMS);
+        MemoryBlockStore store = new MemoryBlockStore(PARAMS);
+
+        // Cheat and place the previous block (block 100000) at the head of the block store without supporting blocks
+        store.put(new StoredBlock(new Block(PARAMS, HEX.decode("0100000050120119172a610421a6c3011dd330d9df07b63616c2cc1f1cd00200000000006657a9252aacd5c0b2940996ecff952228c3067cc38d4885efb5a4ac4247e9f337221b4d4c86041b0f2b5710")),
+                BigInteger.valueOf(1), 100000));
+        store.setChainHead(store.get(Sha256Hash.wrap("000000000003ba27aa200b1cecaad478d2b00432346c3f1f3986da1afd33e506")));
+
+        KeyChainGroup group = new KeyChainGroup(PARAMS);
+        group.importKeys(ECKey.fromPublicOnly(HEX.decode("04b27f7e9475ccf5d9a431cb86d665b8302c140144ec2397fce792f4a4e7765fecf8128534eaa71df04f93c74676ae8279195128a1506ebf7379d23dab8fca0f63")),
+                ECKey.fromPublicOnly(HEX.decode("04732012cb962afa90d31b25d8fb0e32c94e513ab7a17805c14c
