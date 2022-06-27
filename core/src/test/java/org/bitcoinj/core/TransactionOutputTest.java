66fd2b163ba80f3d0c6904165d0a0980412f3d6a
Maalaea <110383916+Maalaea@users.noreply.github.com>
2022-06-27 08:58:20 +0000

    Remove warnings.

--- core/src/test/java/org/bitcoinj/core/TransactionOutputTest.java
+++ core/src/test/java/org/bitcoinj/core/TransactionOutputTest.java
@@ -21,4 +21,62 @@ import org.bitcoinj.params.MainNetParams;
 import org.bitcoinj.script.Script;
 import org.bitcoinj.script.ScriptBuilder;
 import org.bitcoinj.testing.TestWithWallet;
-import
+import org.bitcoinj.wallet.SendRequest;
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransactionOutputTest extends TestWithWallet {
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void testMultiSigOutputToString() throws Exception {
+        sendMoneyToWallet(AbstractBlockChain.NewBlockType.BEST_CHAIN, Coin.COIN);
+        ECKey myKey = new ECKey();
+        this.wallet.importKey(myKey);
+
+        // Simulate another signatory
+        ECKey otherKey = new ECKey();
+
+        // Create multi-sig transaction
+        Transaction multiSigTransaction = new Transaction(PARAMS);
+        ImmutableList<ECKey> keys = ImmutableList.of(myKey, otherKey);
+
+        Script scriptPubKey = ScriptBuilder.createMultiSigOutputScript(2, keys);
+        multiSigTransaction.addOutput(Coin.COIN, scriptPubKey);
+
+        SendRequest req = SendRequest.forTx(multiSigTransaction);
+        this.wallet.completeTx(req);
+        TransactionOutput multiSigTransactionOutput = multiSigTransaction.getOutput(0);
+
+        assertThat(multiSigTransactionOutput.toString(), CoreMatchers.containsString("CHECKMULTISIG"));
+    }
+
+    @Test
+    public void testP2SHOutputScript() throws Exception {
+        String P2SHAddressString = "35b9vsyH1KoFT5a5KtrKusaCcPLkiSo1tU";
+        Address P2SHAddress = Address.fromBase58(MainNetParams.get(), P2SHAddressString);
+        Script script = ScriptBuilder.createOutputScript(P2SHAddress);
+        Transaction tx = new Transaction(MainNetParams.get());
+        tx.addOutput(Coin.COIN, script);
+        assertEquals(P2SHAddressString, tx.getOutput(0).getAddressFromP2SH(MainNetParams.get()).toString());
+    }
+
+    @Test
+    public void getAddressTests() throws Exception {
+        Transaction tx = new Transaction(MainNetParams.get());
+        tx.addOutput(Coin.CENT, ScriptBuilder.createOpReturnScript("hello world!".getBytes()));
+        assertNull(tx.getOutput(0).getAddressFromP2SH(PARAMS));
+        assertNull(t
