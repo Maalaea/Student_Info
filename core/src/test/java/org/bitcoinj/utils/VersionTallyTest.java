9a2aa8a76c04d6a422b436a08ca47a75a5bdc16a
Maalaea <110383916+Maalaea@users.noreply.github.com>
2022-08-17 19:45:14 +0000

    Update navigation

--- core/src/test/java/org/bitcoinj/utils/VersionTallyTest.java
+++ core/src/test/java/org/bitcoinj/utils/VersionTallyTest.java
@@ -8,4 +8,73 @@
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under 
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.bitcoinj.utils;
+
+import org.bitcoinj.core.BlockChain;
+import org.bitcoinj.core.Context;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.core.StoredBlock;
+import org.bitcoinj.params.UnitTestParams;
+import org.bitcoinj.store.BlockStore;
+import org.bitcoinj.store.BlockStoreException;
+import org.bitcoinj.store.MemoryBlockStore;
+import org.bitcoinj.testing.FakeTxBuilder;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+public class VersionTallyTest {
+    private static final NetworkParameters PARAMS = UnitTestParams.get();
+
+    public VersionTallyTest() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        BriefLogFormatter.initVerbose();
+        Context context = new Context(PARAMS);
+    }
+
+    /**
+     * Verify that the version tally returns null until it collects enough data.
+     */
+    @Test
+    public void testNullWhileEmpty() {
+        VersionTally instance = new VersionTally(PARAMS);
+        for (int i = 0; i < PARAMS.getMajorityWindow(); i++) {
+            assertNull(instance.getCountAtOrAbove(1));
+            instance.add(1);
+        }
+        assertEquals(PARAMS.getMajorityWindow(), instance.getCountAtOrAbove(1).intValue());
+    }
+
+    /**
+     * Verify that the size of the version tally matches the network parameters.
+     */
+    @Test
+    public void testSize() {
+        VersionTally instance = new VersionTally(PARAMS);
+        assertEquals(PARAMS.getMajorityWindow(), instance.size());
+    }
+
+    /**
+     * Verify that version count and substitution works correctly.
+     */
+    @Test
+    public void testVersionCounts() {
+        VersionTally instance = new VersionTally(PARAMS);
+
+        // Fill the tally with 1s
+        for (int i = 0; i < PARAMS.getMajorityWindow(); i++) {
+            assertNull(instance.getCountAtOrAbove(1));
+            instance.add(1);
+        }
+        assertEquals(PARAMS.getMajorityWindow(), instance.getCountAtOrAbove(1).intValue());
+
+        // Check the count updates as we repla
