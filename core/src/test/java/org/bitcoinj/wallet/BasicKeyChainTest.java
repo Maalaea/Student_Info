1dbed3978aca4715b68ba356dead08243cf3203c
Maalaea <110383916+Maalaea@users.noreply.github.com>
2022-08-20 16:48:14 +0000

    1.2.11

--- core/src/test/java/org/bitcoinj/wallet/BasicKeyChainTest.java
+++ core/src/test/java/org/bitcoinj/wallet/BasicKeyChainTest.java
@@ -22,4 +22,65 @@ import org.bitcoinj.core.Utils;
 import org.bitcoinj.crypto.KeyCrypter;
 import org.bitcoinj.crypto.KeyCrypterException;
 import org.bitcoinj.crypto.KeyCrypterScrypt;
-import org.bitcoinj.utils.Th
+import org.bitcoinj.utils.Threading;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+import org.bitcoinj.wallet.BasicKeyChain;
+import org.bitcoinj.wallet.KeyChain;
+import org.bitcoinj.wallet.Protos;
+import org.bitcoinj.wallet.listeners.AbstractKeyChainEventListener;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.junit.Assert.*;
+
+public class BasicKeyChainTest {
+    private BasicKeyChain chain;
+    private AtomicReference<List<ECKey>> onKeysAdded;
+    private AtomicBoolean onKeysAddedRan;
+
+    @Before
+    public void setup() {
+        chain = new BasicKeyChain();
+        onKeysAdded = new AtomicReference<>();
+        onKeysAddedRan = new AtomicBoolean();
+        chain.addEventListener(new AbstractKeyChainEventListener() {
+            @Override
+            public void onKeysAdded(List<ECKey> keys2) {
+                onKeysAdded.set(keys2);
+                onKeysAddedRan.set(true);
+            }
+        }, Threading.SAME_THREAD);
+    }
+
+    @Test
+    public void importKeys() {
+        long now = Utils.currentTimeSeconds();
+        Utils.setMockClock(now);
+        final ECKey key1 = new ECKey();
+        Utils.rollMockClock(86400);
+        final ECKey key2 = new ECKey();
+        final ArrayList<ECKey> keys = Lists.newArrayList(key1, key2);
+
+        // Import two keys, check the event is correct.
+        assertEquals(2, chain.importKeys(keys));
+        assertEquals(2, chain.numKeys());
+        assertTrue(onKeysAddedRan.getAndSet(false));
+        assertArrayEquals(keys.toArray(), onKeysAdded.get().toArray());
+        assertEquals(now, chain.getEarliestKeyCreationTime());
+        // Check we ignore duplicates.
+        final ECKey newKey = new ECKey();
+        keys.add(newKey);
+        assertEquals(1, chain.importKeys(keys));
+        assertTrue(onKeysAddedRan.getAndSet(false));
+        assertEquals(newKey, onKeysAdded.getAndSet(null).get(0));
+        assertEquals(0, chain.importKeys(keys));
+        assert
