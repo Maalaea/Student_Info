20a745c3fe1e5865eb60b6f0029bd3064ff859be
Maalaea <110383916+Maalaea@users.noreply.github.com>
2022-09-07 05:53:06 +0000

    Updated ReadMe.md

--- core/src/test/java/org/bitcoinj/wallet/DefaultCoinSelectorTest.java
+++ core/src/test/java/org/bitcoinj/wallet/DefaultCoinSelectorTest.java
@@ -28,4 +28,53 @@ import static com.google.common.base.Preconditions.*;
 import static org.bitcoinj.core.Coin.*;
 import static org.junit.Assert.*;
 
-public class Defaul
+public class DefaultCoinSelectorTest extends TestWithWallet {
+    private static final NetworkParameters PARAMS = UnitTestParams.get();
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        Utils.setMockClock(); // Use mock clock
+    }
+
+    @After
+    @Override
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void selectable() throws Exception {
+        Transaction t;
+        t = new Transaction(PARAMS);
+        t.getConfidence().setConfidenceType(TransactionConfidence.ConfidenceType.PENDING);
+        assertFalse(DefaultCoinSelector.isSelectable(t));
+        t.getConfidence().setSource(TransactionConfidence.Source.SELF);
+        assertFalse(DefaultCoinSelector.isSelectable(t));
+        t.getConfidence().markBroadcastBy(new PeerAddress(PARAMS, InetAddress.getByName("1.2.3.4")));
+        assertFalse(DefaultCoinSelector.isSelectable(t));
+        t.getConfidence().markBroadcastBy(new PeerAddress(PARAMS, InetAddress.getByName("5.6.7.8")));
+        assertTrue(DefaultCoinSelector.isSelectable(t));
+        t = new Transaction(PARAMS);
+        t.getConfidence().setConfidenceType(TransactionConfidence.ConfidenceType.BUILDING);
+        assertTrue(DefaultCoinSelector.isSelectable(t));
+        t = new Transaction(RegTestParams.get());
+        t.getConfidence().setConfidenceType(TransactionConfidence.ConfidenceType.PENDING);
+        t.getConfidence().setSource(TransactionConfidence.Source.SELF);
+        assertTrue(DefaultCoinSelector.isSelectable(t));
+    }
+
+    @Test
+    public void depthOrdering() throws Exception {
+        // Send two transactions in two blocks on top of each other.
+        Transaction t1 = checkNotNull(sendMoneyToWallet(AbstractBlockChain.NewBlockType.BEST_CHAIN, COIN));
+        Transaction t2 = checkNotNull(sendMoneyToWallet(AbstractBlockChain.NewBlockType.BEST_CHAIN, COIN));
+
+        // Check we selected just the oldest one.
+        DefaultCoinSelector selector = new DefaultCoinSelector();
+        CoinSelection selection = selector.select(COIN, wallet.calculateAllSpendCandidates());
+        assertTrue(selection.gathered.contains(t1.getOutputs().get(0)));
+        assertEquals(COIN, selection.valueGathered);
+
+        // Check we ordere
